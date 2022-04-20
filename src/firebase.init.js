198ac190350e8f42acd952e4411c0083d65c76d0
Shakil00077 <shakilshehjaad@gmail.com>
2022-04-20 16:31:09 +0600

    email verification requirment set

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyChhxG9Z2OYqFUAVBs0JLolpwlo_mNBJlA",
+  authDomain: "genius-car-service2-b6ff6.firebaseapp.com",
+  projectId: "genius-car-service2-b6ff6",
+  storageBucket: "genius-car-service2-b6ff6.appspot.com",
+  messagingSenderId: "402349214942",
+  appId: "1:402349214942:web:00d3f238acc5bd2a60c64f"
 };
 
 // Initialize Firebase
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <HelmetProvider>
+    <BrowserRouter>
+     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+     </React.StrictMode>
+   </BrowserRouter>
+  </HelmetProvider>
   ,
   document.getElementById('root')
 );
